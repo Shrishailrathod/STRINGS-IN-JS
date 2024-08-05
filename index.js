07e7021f3327318033fb69675bfc0c6ecf2490c9
shrishail Rathod <112097349+Shrishailrathod@users.noreply.github.com>
2024-08-05 09:16:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// javascript Methods
+let a = "Rathod";
+ console.log(a.toUpperCase());
+ console.log(a.toLowerCase());
+ console.log(a.slice(1,4));
+ console.log(a.replace("Ra","12"));
+
+
+
+
+// template literals
+// template literals provoides more flexible way to create a sctrings,allowing embedded expression and multiline strings
+let real_name = "Rathod";
+let age = 30;
+console.log("hello, my Name is " +real_name + " and my age is " +age); 
+console.log(`hello, my name is ${real_name} and my age is ${age}`);
+
+
+
+
+// concatination in strings can be done  by using "+" operator
+let firstname = "hello";
+let lastname = "Rathod";
+console.log(firstname+ ' '+lastname);
+
+
+
+
+console.log("I am Rathod");
+
+
+// Accessing charecters
+let a = "Rathod";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+
+// string length
+console.log(a.length)
